364f3cf311c3628ca30d9d1d2a3715b019b24bc7
Daniel Gamboa Estrada <daniel.gamboa@boats.com>
2024-11-21 10:55:27 -0600

    Add structure of the project

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  description = "The region in which the resources will be deployed"
+  default     = "us-east-1"
+}
+
+variable "profile" {
+  description = "The AWS profile to use"
+}
